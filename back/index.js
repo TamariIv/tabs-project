b9cab63d75416442553d56c044e9f92ad1feaab4
TamariIv <taivgi@g.jct.ac.il>
2024-10-29 13:19:40 +0200

    Initial project setup

---
+++ back/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.json());
+
+const db = [
+    { id: 1, color: "orange", text: "Lorem ipsum dolor sit amet" },
+    { id: 2, color: "purple", text: "Ut enim ad minim veniam" },
+    { id: 3, color: "green", text: "Sed do eiusmod tempor incididunt" },
+    { id: 4, color: "blue", text: "Ut labore et dolore magna aliqua" }
+];
+// Define a basic route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/tabs', (req, res) => {
+    res.json(db);
+});
+
+app.get('/tabs/:id', (req, res) => {
+    const id = req.params.id;
+    const tab = db.find(tab => tab.id === parseInt(id));
+    if (!tab) {
+        return res.status(404).send('Tab not found');
+    }
+    res.json(tab);
+});
+
+app.post('/tabs', (req, res) => {
+    const tab = {
+        id: db.length + 1,
+        color: req.body.color,
+        text: req.body.text
+    };
+    db.push(tab);
+    res.json(tab);
+});
+
+
+app.patch('/tabs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const tabIndex = db.findIndex((tab) => tab.id === id);
+
+    if (tabIndex !== -1) {
+        db[tabIndex] = { ...db[tabIndex], ...req.body };
+        res.json(db[tabIndex]);
+    } else {
+        res.status(404).json({ message: 'Tab not found' });
+    }
+});
+
+app.delete('/tabs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const tabIndex = db.findIndex((tab) => tab.id === id);
+
+    if (tabIndex !== -1) {
+        const deletedTab = db.splice(tabIndex, 1);
+        res.json(deletedTab);
+    } else {
+        res.status(404).json({ message: 'Tab not found' });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
